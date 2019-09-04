fda66a24f2b5dad451e4f126397e78a45bf948ef
Andrei Buryak <ottone322@gmail.com>
2019-09-04 10:36:51 +0300

    First Changed ("Hello World"). Signed-off-by: Andrei Buryak <ottone322@gmail.com>

---
+++ Зад3.Switchcase/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Зад3.Switchcase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 8;
+            bool z = (5 < 6) & (x % 2 == 0);
+            Console.WriteLine(z);
+        }
+    }
+}
