070db7834d124b7e4d876ba91521c3a003b9a383
Andrei Buryak <ottone322@gmail.com>
2019-09-04 11:26:16 +0300

    Changed grammar Signed-off-by: Andrei Buryak <ottone322@gmail.com>

--- Зад3.Switchcase/Program.cs
+++ Зад3.Switchcase/Program.cs
@@ -13,7 +13,7 @@ namespace Зад3.Switchcase
             string name;
             Console.WriteLine("Enter your name: ");
             name = Console.ReadLine();
-            Console.WriteLine("Hello {0}", name);
+            Console.WriteLine("Hello , {0}", name);
             Console.ReadLine();
         }
     }
